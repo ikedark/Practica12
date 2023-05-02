b74a0dd88ce059954848fe88c91ea06a88215205
ikedark <david.v1795@gmail.com>
2023-05-02 12:43:07 -0700

    x

--- app/src/main/java/mx/edu/potros/practica12/MainActivity.kt
+++ app/src/main/java/mx/edu/potros/practica12/MainActivity.kt
@@ -1,16 +1,29 @@
 package mx.edu.potros.practica12
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.database
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class MainActivity : AppCompatActivity() {
     private val File = 1
     private val database = Firebase.database
+    val myRef = database.getReference("user")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val uploadImageView: ImageView = findViewById(R.id.uploadImageView)
+        uploadImageView.setOnClickListener {
+            fileUpload()
+        }
+    }
+    fun fileUpload(){
+        val intent= Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
     }
 }
